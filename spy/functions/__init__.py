0c8906ec1892185c8646793921e5137cd3821054
liszt90 <liszt90@stanford.edu>
2011-05-22 19:33:08 +0000

    added min

--- spy/functions/__init__.py
+++ spy/functions/__init__.py
@@ -1,14 +1,15 @@
-from exp import exp
-from max import max
-from sum import sum
-from prod import prod
 from abs import abs
+from exp import exp
 from log import log
+from max import max
+from min import min
+from sum import sum
 from power import power
+from prod import prod
+from quad_over_lin import quad_over_lin
 from sqrt import sqrt
 from square import square
-from quad_over_lin import quad_over_lin
 
-__all__ = ["exp", "max", "sum", "prod",
-           "abs", "log", "power", "sqrt",
-           "square", "quad_over_lin"]
+__all__ = ["abs", "exp", "log", "max",
+           "min", "sum", "power", "prod",
+           "quad_over_lin", "sqrt", "square"]

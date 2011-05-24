708c3c3c0266412caf20b4d2c79b4a137c762b4e
liszt90@stanford.edu <liszt90@stanford.edu@10b414d9-af7b-62dd-af0a-746aad51e1b2>
2011-05-24 16:53:25 +0000

    added l1 norm and l2 norm

--- trunk/spy/functions/__init__.py
+++ trunk/spy/functions/__init__.py
@@ -4,6 +4,8 @@ from huber import huber
 from log import log
 from max import max
 from min import min
+from norm1 import norm1
+from norm2 import norm2
 from pos import pos
 from power import power
 from quad_over_lin import quad_over_lin
@@ -11,5 +13,6 @@ from sqrt import sqrt
 from square import square
 
 __all__ = ["abs", "exp", "huber", "log",
-           "max", "min", "pos", "power",
-           "quad_over_lin", "sqrt", "square"]
+           "max", "min", "norm1", "norm2",
+           "pos", "power", "quad_over_lin",
+           "sqrt", "square"]

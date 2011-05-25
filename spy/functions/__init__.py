5bb86dfa05f0956983ae26d87411df92e00e4ebd
mauriziocalo07 <mauriziocalo07@gmail.com>
2011-05-25 06:57:24 +0000

    Added testing code for DCP ruleset, atoms..also a few other atoms..Still having trouble importing spy/organizing package

--- spy/functions/__init__.py
+++ spy/functions/__init__.py
@@ -1,19 +1,24 @@
 from abs import abs
 from berhu import berhu
 from exp import exp
+from geo_mean import geo_mean
 from huber import huber
 from log import log
+from log_sum_exp import log_sum_exp
 from max import max
 from min import min
 from norm1 import norm1
 from norm2 import norm2
 from pos import pos
 from power import power
+from power_pos import power_pos
 from quad_over_lin import quad_over_lin
+from rel_entr import rel_entr
 from sqrt import sqrt
 from square import square
+from square_pos import square_pos
 
-__all__ = ["abs", "berhu", "exp", "huber",
+__all__ = ["abs", "berhu", "exp", "huber","geo_mean",
            "log", "max", "min", "norm1",
-           "norm2", "pos", "power",
-           "quad_over_lin", "sqrt", "square"]
+           "norm2", "log_sum_exp","pos", "power","power_pos",
+           "quad_over_lin", "rel_entr","sqrt", "square","square_pos"]
